95e5455c9f0c4c9026f29d5f0855810c4d19cc5f
KouYunPeng <kyp5146@126.com>
2021-09-27 12:33:43 +0800

    修改版本

--- Example/MWPhotoBrowser/Menu.m
+++ Example/MWPhotoBrowser/Menu.m
@@ -21,7 +21,7 @@
 		self.title = @"MWPhotoBrowser";
         
         // Clear cache for testing
-        [[SDImageCache sharedImageCache] clearDisk];
+        [[SDImageCache sharedImageCache] clearDiskOnCompletion:nil];
         [[SDImageCache sharedImageCache] clearMemory];
         
         _segmentedControl = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"Push", @"Modal", nil]];
@@ -1274,7 +1274,9 @@
                 [_assets addObject:obj];
             }];
             if (fetchResults.count > 0) {
-                [self.tableView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self.tableView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
+                });
             }
         });
         
